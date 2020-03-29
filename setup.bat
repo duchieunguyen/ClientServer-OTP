a892df40a53fa2fca751fb76629f1a4416a7f4f8
Nguyen Duc Hieu <duchieunb7795@gmail.com>
2020-03-29 12:45:44 +0700

    Revert "Revert "Commit first version""
    This reverts commit 66fbc6eb4cf1cf363b02eaf8a31ff969d6a33742.

---
+++ setup.bat
@@ -0,0 +1,11 @@
+#!/bin/bash
+@echo on
+pip install socket
+pip install sys
+pip install configparser
+pip install os
+pip install time
+pip install json
+pip install math
+pip install random
+pip install threading
